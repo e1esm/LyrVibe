d2d16b75a36bab1616c8a7afa2341a38776c100f
elesm <elesmdev1102@gmail.com>
2023-08-14 06:27:39 +0300

    Created grpc files for artitst-service API

--- artist-service/internal/models/artist.go
+++ artist-service/internal/models/artist.go
@@ -1,7 +1,7 @@
 package models
 
 type Artist struct {
-	Username   string
+	Username   string `json:"username"`
 	Country    string `json:"country" `
 	FirstName  string `json:"first_name"`
 	SecondName string `json:"second_name"`
