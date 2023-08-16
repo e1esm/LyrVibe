cd90af06a48f990877ba32ef91659bec6b72509e
elesm <elesmdev1102@gmail.com>
2023-08-16 16:09:57 +0300

    minor code fixes

--- gateway/internal/server/proxy.go
+++ gateway/internal/server/proxy.go
@@ -2,15 +2,10 @@ package server
 
 import (
 	"errors"
-	"fmt"
-	"github.com/e1esm/LyrVibe/auth-service/api/v1/proto"
 	"github.com/e1esm/LyrVibe/gateway/internal/service"
 	"github.com/e1esm/LyrVibe/gateway/pkg/logger"
 	"github.com/gin-gonic/gin"
 	"go.uber.org/zap"
-	"net/http"
-	"strings"
-	"time"
 )
 
 var (
@@ -46,87 +41,4 @@ func (ps *ProxyServer) Run(address string) {
 		logger.Logger.Fatal("Couldn't have started the server",
 			zap.String("err", err.Error()))
 	}
-}
-
-func (ps *ProxyServer) Login(c *gin.Context) {
-	var signInRequest proto.SignInRequest
-	if err := c.BindJSON(&signInRequest); err != nil {
-		c.JSON(http.StatusBadRequest, reqBodyErr)
-		return
-	}
-	resp, err := ps.Services.AuthService.Login(&signInRequest)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, err)
-		return
-	}
-	accessTTL, err := time.ParseDuration(resp.Tokens.AccessTTL)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, TTLErr)
-		return
-	}
-	refreshTTL, err := time.ParseDuration(resp.Tokens.RefreshTTL)
-	if err != nil {
-		logger.Logger.Info(err.Error())
-		c.JSON(http.StatusInternalServerError, TTLErr)
-		return
-	}
-	http.SetCookie(c.Writer, &http.Cookie{
-		Name:     "access_token",
-		Value:    resp.Tokens.AccessToken,
-		Expires:  time.Now().Add(accessTTL),
-		HttpOnly: true,
-		Path:     "/",
-	})
-	http.SetCookie(c.Writer, &http.Cookie{
-		Name:     "refresh_token",
-		Value:    resp.Tokens.RefreshToken,
-		Expires:  time.Now().Add(refreshTTL),
-		HttpOnly: true,
-		Path:     "/",
-	})
-	c.JSON(http.StatusOK, LogInOk)
-}
-
-func (ps *ProxyServer) SignUp(c *gin.Context) {
-	signUpRequest := proto.SignUpRequest{}
-	if err := c.BindJSON(&signUpRequest); err != nil {
-		logger.Logger.Error(err.Error())
-		c.JSON(http.StatusBadRequest, reqBodyErr)
-		return
-	}
-
-	resp, err := ps.Services.AuthService.SignUp(&signUpRequest)
-	if err != nil {
-		logger.Logger.Info(fmt.Sprintf("Password: %v", signUpRequest.Password))
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"username": signUpRequest.Username,
-			"error":    err.Error(),
-		})
-		return
-	}
-	c.JSON(http.StatusOK, resp)
-}
-
-func (ps *ProxyServer) Logout(c *gin.Context) {
-	token, err := c.Cookie("access_token")
-	if err != nil {
-		c.JSON(http.StatusBadRequest, cookieRetrievingErr)
-	}
-	err = ps.Services.AuthService.Logout(&proto.LogoutRequest{AccessToken: token})
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, err)
-		return
-	}
-	c.JSON(http.StatusOK, successLogOut)
-
-}
-
-func (ps *ProxyServer) AuthMiddleware(c *gin.Context) {
-	if strings.Contains(c.Request.RequestURI, "logout") {
-		if token, err := c.Cookie("access_token"); err != nil || token == "" {
-			c.JSON(http.StatusUnauthorized, "Unauthorized")
-			return
-		}
-	}
-	c.Next()
 }
